85cae96edbfb97631a8508d63337024442b448ee
Movieats-devops <devops.movieats@gmail.com>
2026-04-21 17:55:28 -0300

    fix: remove old r2 domain and enforce cdn.softcloudba.com

--- movieats-business/next.config.ts
+++ movieats-business/next.config.ts
@@ -6,12 +6,6 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'pub-d84f04dc65734e04aeac5ca2.r2.dev',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
         hostname: 'cdn.softcloudba.com',
         port: '',
         pathname: '/**',
