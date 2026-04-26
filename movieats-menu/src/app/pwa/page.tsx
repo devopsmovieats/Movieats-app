1600a57145910ec4ae12f3432a78b986b81a2121
Movieats-devops <devops.movieats@gmail.com>
2026-04-26 16:23:49 -0300

    debug: fix typo in pwa test page

--- movieats-menu/src/app/pwa/page.tsx
+++ movieats-menu/src/app/pwa/page.tsx
@@ -1,6 +1,6 @@
 export default function PwaPage() {
   return (
-    <div style={{ background: 'black', color: 'white', minHeight: '100vh', display: 'flex', alignItems: 'center', justifyCenter: 'center' }}>
+    <div style={{ background: 'black', color: 'white', minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
       <h1>TESTE PWA - ROTA ATIVA</h1>
     </div>
   );
