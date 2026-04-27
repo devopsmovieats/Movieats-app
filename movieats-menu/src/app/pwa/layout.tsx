62fda0433d24a3b7001cd9c0ee7bb5c502f63eb9
Movieats-devops <devops.movieats@gmail.com>
2026-04-27 12:00:47 -0300

    fix: Movido UserProvider para o layout raiz para suportar a tela de identificação na homepage e corrigido layout do PWA

--- movieats-menu/src/app/pwa/layout.tsx
+++ movieats-menu/src/app/pwa/layout.tsx
@@ -1,18 +1,11 @@
-"use client";
-
-import { UserProvider } from "@/context/UserContext";
-import "./globals.css";
-
 export default function PWALayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <UserProvider>
-      <div className="min-h-screen bg-transparent text-white selection:bg-primary/20">
-        {children}
-      </div>
-    </UserProvider>
+    <div className="min-h-screen bg-transparent text-white selection:bg-primary/20">
+      {children}
+    </div>
   );
 }
